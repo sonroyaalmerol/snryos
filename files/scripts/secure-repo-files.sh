1ad1f9f6e367ed60dce00387bf0e7670355b02b2
Son Roy Almerol <github@snry.me>
2025-03-15 23:44:31 +0000

    use run0 sh for chcon

--- files/scripts/secure-repo-files.sh
+++ files/scripts/secure-repo-files.sh
@@ -2,6 +2,11 @@
 
 set -oue pipefail
 
-chown root:root /etc/yum.repos.d/*
-chmod 744 /etc/yum.repos.d/*
-chcon -R system_u:object_r:system_conf_t:s0 /etc/yum.repos.d/
+echo "Securing the repo files against manipulation."
+run0 sh -c "chown root:root /etc/yum.repos.d/* && \
+                chmod 744 /etc/yum.repos.d/* && \
+                chcon -R system_u:object_r:system_conf_t:s0 /etc/yum.repos.d/
+            " || {
+    echo "Failed to secure the repo files!"
+    exit 1
+}
