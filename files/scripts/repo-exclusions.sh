64053740d4f2fa44a9e91d8346a11c80d5ab3359
Son Roy Almerol <github@snry.me>
2025-03-16 03:59:54 +0000

    add more repo replacements

---
+++ files/scripts/repo-exclusions.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+dnf5 -y config-manager setopt "*bazzite*".priority=1
+dnf5 -y config-manager setopt "*akmods*".priority=2
+dnf5 -y config-manager setopt "*terra*".priority=3 "*terra*".exclude="nerd-fonts topgrade"
+dnf5 -y config-manager setopt "terra-mesa".enabled=true
+dnf5 -y config-manager setopt "terra-nvidia".enabled=false
+dnf5 -y config-manager setopt "*rpmfusion*".priority=5 "*rpmfusion*".exclude="mesa-*"
+dnf5 -y config-manager setopt "*fedora*".exclude="mesa-* kernel-core-* kernel-modules-* kernel-uki-virt-*"
+dnf5 -y config-manager setopt "*staging*".exclude="scx-scheds kf6-* mesa* mutter* rpm-ostree* systemd* gnome-shell gnome-settings-daemon gnome-control-center gnome-software libadwaita tuned*"
