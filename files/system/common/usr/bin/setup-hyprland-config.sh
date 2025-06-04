192aea9d2b8beb440079f576451c85e7029fee68
Son Roy Almerol <github@snry.me>
2025-06-04 11:14:48 -0400

    switch to uwsm hyprland

--- files/system/common/usr/bin/setup-hyprland-config.sh
+++ files/system/common/usr/bin/setup-hyprland-config.sh
@@ -7,5 +7,6 @@ mkdir -p "$CONFIG_DIR"
 # Clone the starter configuration
 if [ ! -f "$CONFIG_DIR/.initialized" ]; then
   cp -rf /etc/xdg/hypr/hyprland.conf "$CONFIG_DIR/hyprland.conf"
+  cp -rf /etc/xdg/hypr/hypridle.conf "$CONFIG_DIR/hypridle.conf"
   touch "$CONFIG_DIR/.initialized"
 fi
