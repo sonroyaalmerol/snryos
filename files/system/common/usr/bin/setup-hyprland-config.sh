b3415b49fe5da765ab009136a2d8ef758975826e
Son Roy Almerol <github@snry.me>
2025-06-06 10:19:45 -0400

    initialize env vars in uwsm instead

--- files/system/common/usr/bin/setup-hyprland-config.sh
+++ files/system/common/usr/bin/setup-hyprland-config.sh
@@ -2,7 +2,9 @@
 
 # Hyprland
 CONFIG_DIR="$HOME/.config/hypr"
+UWSM_DIR="$HOME/.config/uwsm"
 mkdir -p "$CONFIG_DIR"
+mkdir -p "$UWSM_DIR"
 
 # Clone the starter configuration
 if [ ! -f "$CONFIG_DIR/.initialized" ]; then
@@ -10,4 +12,10 @@ if [ ! -f "$CONFIG_DIR/.initialized" ]; then
   cp -rf /etc/xdg/hypr/hypridle.conf "$CONFIG_DIR/hypridle.conf"
   cp -rf /etc/zshrc "$HOME/.zshrc"
   touch "$CONFIG_DIR/.initialized"
+fi
+
+if [ ! -f "$UWSM_DIR/.initialized" ]; then
+  cp -rf /etc/xdg/uwsm/env "$UWSM_DIR/env"
+  cp -rf /etc/xdg/uwsm/env-hyprland "$UWSM_DIR/env-hyprland"
+  touch "$UWSM_DIR/.initialized"
 fi
